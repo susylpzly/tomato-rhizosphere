8cf93c0a72ee563dbf1d861571b41389b7fc7bf0
susylpzly <s.lopezlemarroy@student.vu.nl.com>
2022-09-22 16:22:52 +0200

    thursday updates

--- gff_filtering.py
+++ gff_filtering.py
@@ -3,40 +3,45 @@
 
 import sys
 
-def filtering(ann_file, ass_file):
-    #Open the files
-    annotations = open(ann_file, "r")
-    assemblies = open(ass_file, "r")
+ann_file = sys.argv[1]
+ass_file = sys.argv[2]
+#out_file = sys.argv[3]
+    
+#Open the files
+annotations = open(ann_file, "r")
+assemblies = open(ass_file, "r")
 
-    #Results in a list of lines, in column organization 
-    out_file = [] 
+new_ann_file = open(sys.argv[3],'w')
+#Results in a list of lines, in column organization 
+new_annotations = [] 
 
-    #Make a list of the contig names in the assembly file
-    for line in assemblies:
-        if line.startswith('>'):
-            contig_ID = line 
+contig_IDs = []
+#Make a list of the contig names in the assembly file
+for line in assemblies:
+    if line.startswith('>'):
+        contig_ID = line 
+        #remove the '>' and the '\n' from the contigID 
+        contig_ID = contig_ID.replace('>','')
+        contig_ID = contig_ID.replace('\n','')
+        contig_IDs.append(contig_ID) 
 
     
-    for line in annotations:
-        #Separate the columns from the .gff file and take first element as value variable
-        columns = line.strip().split('\t')
-        value = columns[0]
-    
+for line in annotations:
+    #Separate the columns from the .gff file and take first element as value variable
+    columns = line.strip().split('\t')
+    value = columns[0]
+    if value in contig_IDs:
+        #add the line to the file 
+        new_annotations.append(line)
+    else:
+        pass 
+
+#Write all the found contigs into the output file 
+for element in new_annotations:
+    new_ann_file.write(element + '\n')
 
 
-
-    #Remeber to close files after done
-    annotations.close()
-    assemblies.close() 
-
-    #Return the filtered gff file 
-    return out_file 
-
-def main():
-    ann_file = sys.argv[1]
-    ass_file = sys.argv[2]
-    out_file = filtering(ann_file,ass_file)
-
-
-if __name__ == '__main__':
-    main()
+#Remeber to close files after done
+annotations.close()
+assemblies.close() 
+new_ann_file.close()
